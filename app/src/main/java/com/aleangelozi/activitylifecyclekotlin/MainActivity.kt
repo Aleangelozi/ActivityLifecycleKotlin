7a1c9545ee9fcaec5456d4c34eb957d53effe874
aleangelozi <aleangelozi@gmail.com>
2021-05-01 21:44:01 -0300

    Migration to View Binding.

--- app/src/main/java/com/aleangelozi/activitylifecyclekotlin/MainActivity.kt
+++ app/src/main/java/com/aleangelozi/activitylifecyclekotlin/MainActivity.kt
@@ -3,14 +3,20 @@ package com.aleangelozi.activitylifecyclekotlin
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.aleangelozi.activitylifecyclekotlin.databinding.ActivityMainBinding
 
-const val TAG = "Activity method"
+private const val TAG = "Activity method"
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         Log.d(TAG, "onCreate")
     }
 
